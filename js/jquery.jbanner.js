8170b96569c9dda62f493bb5835d1923b7156110
MorganShowman <email@mshowman.com>
2010-02-16 17:03:08 -0500

    added margin and border settings

--- js/jquery.jbanner.js
+++ js/jquery.jbanner.js
@@ -16,13 +16,17 @@
 	$.jBanner = {
 		defaults: {
 			bannerContainer: "",
-			height: 295,		// height of images
-			width: 685, 		// width of images
-			padding: 5, 		// amount of padding (in pixels) between images
-			caption: false,		// display caption? true or false
-			cheight: 35, 		// caption height
-			delay: 5000, 		// delay to next element
-			speed: 1000 		// transition speed
+			height: 295,			// height of images
+			width: 685, 			// width of images
+			borderSize: '0',			// size of border around images
+			borderStyle: 'none',	// style of border around images
+			borderColor: 'F00',		// color of border around images
+			padding: 0, 			// amount of padding (in pixels) around images
+			margin: 0, 				// amount of margin (in pixels) around images
+			caption: false,			// display caption? true or false
+			cheight: 0, 			// caption height
+			delay: 5000, 			// delay to next element
+			speed: 1000 			// transition speed
 		}
 	};
 	
@@ -44,24 +48,24 @@
 		}
 	});
 	function banner(config) {
-		$(config.bannerContainer).css({'position':'relative','height':config.height+config.cheight+'px','width':config.width+'px','overflow':'hidden'});
-		$(config.bannerContainer+" li").each(function(i){$(this).css({'position':'absolute','top':'0','left':i*(config.width+config.padding)+'px'});$(this).attr("title",i+1);$(this).children("a").after("<p>"+$(this).children("a").children("img").attr("alt")+"</p>");});
+		$(config.bannerContainer).css({'position':'relative','height':config.height+config.cheight+config.padding*2+config.margin*2+parseInt(config.borderSize)*2+'px','width':config.width+config.padding*2+parseInt(config.borderSize)*2+config.margin*2+'px','overflow':'hidden'});
+		$(config.bannerContainer+" li").each(function(i){$(this).css({'position':'absolute','top':'0','left':i*(config.width+config.padding*2+config.margin*2+parseInt(config.borderSize)*2)+'px'});$(this).attr("title",i+1);$(this).children("a").after("<p>"+$(this).children("a").children("img").attr("alt")+"</p>");});
 		$(config.bannerContainer+" li:first").addClass("selected");
-		$(config.bannerContainer+" li img").css({'height':config.height,'width':config.width});
+		$(config.bannerContainer+" li img").css({'border':config.borderSize+'px '+config.borderStyle+' #'+config.borderColor,'height':config.height,'width':config.width});
 		setTimeout( function(){next(config)}, config.delay);
 	};
 	function next(config) {
 		var i = parseInt($(config.bannerContainer+" li.selected").attr("title"));
-		if (LastElement(config,i)){$(config.bannerContainer+" li[title='"+NextElement(config,i)+"']").css('left',(config.width+config.padding)+'px');}
+		if (LastElement(config,i)){$(config.bannerContainer+" li[title='"+NextElement(config,i)+"']").css('left',(config.width+config.padding*2+config.margin*2+parseInt(config.borderSize)*2)+'px');}
 		$(config.bannerContainer+" li[title='"+i+"']").removeClass("selected");
 		$(config.bannerContainer+" li[title='"+(NextElement(config,i))+"']").addClass("selected");
-		$(config.bannerContainer+" li").each(function(i){var newLeft = parseInt( $(this).css("left").replace(/px/i, '') )-(config.width+config.padding);$(this).animate({'left':newLeft+'px'},config.speed+250);});
+		$(config.bannerContainer+" li").each(function(i){var newLeft = parseInt( $(this).css("left").replace(/px/i, '') )-(config.width+config.padding*2+config.margin*2+parseInt(config.borderSize)*2);$(this).animate({'left':newLeft+'px'},config.speed+250);});
 		setTimeout( function(){next(config)}, config.delay);
 	};
 	function NextElement(config,index) {
 		if(index < parseInt($(config.bannerContainer+" li:last").attr("title"))) {return index + 1;} else {return 1;}
 	};
 	function LastElement(config,index) {
-		return (parseInt($(config.bannerContainer+" li[title='"+NextElement(config,index)+"']").css("left").replace(/px/i, '')) == (1 - parseInt($(config.bannerContainer+" li:last").attr("title")))*(config.width+config.padding));
+		return (parseInt($(config.bannerContainer+" li[title='"+NextElement(config,index)+"']").css("left").replace(/px/i, '')) == (1 - parseInt($(config.bannerContainer+" li:last").attr("title")))*(config.width+config.padding*2+config.margin*2+parseInt(config.borderSize)*2));
 	};
 })(jQuery);
